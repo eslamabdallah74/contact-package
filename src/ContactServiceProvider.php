bd4cd1b948cc33e4b9f50c0d7476eec47ca8c0ca
eslamabdallah74 <eslamabdallah301@gmail.com>
2022-09-23 03:30:00 +0200

    publish files

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -12,9 +12,14 @@ class ContactServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         $this->loadViewsFrom(__DIR__ . '/views', 'contact');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-        $this->mergeConfigFrom(
-            __DIR__.'/config/contact.php','contact'
-        );
+        $this->mergeConfigFrom(__DIR__.'/config/contact.php','contact');
+        // Publish files
+        $this->publishes([__DIR__ . '/config/contact.php' => config_path('contact.php')]);
+        $this->publishes([
+            __DIR__ . '/config/contact.php' => config_path('contact.php'),
+            __DIR__ . '/views' => resource_path('views/vendor/contact'),
+        ]);
+
 
     }
 
